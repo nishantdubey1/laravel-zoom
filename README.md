0e007526fb3c2e9df5dae14f97aa5bb90298587f
nishantdubey21 <nishant.dubey@ditinustechnology.com>
2022-11-03 12:29:57 +0530

    Laravel Zoom App

---
+++ README.md
@@ -0,0 +1,7 @@
+# Laravel Zoom API integration with server to server auth
+
+Laravel API Integration with zoom API
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ config/laravelzoom.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'nishant_zoom_account_id' => env('NISHANT_ZOOM_ACCOUNT_ID'),
+	'nishant_zoom_client_id' => env('NISHANT_ZOOM_CLIENT_ID'),
+	'nishant_zoom_client_secret' => env('NISHANT_ZOOM_CLIENT_SECRET'),
+    'nishant_zoom_api_key' => env('NISHANT_ZOOM_API_KEY', ''),
+    'nishant_zoom_api_secret' => env('NISHANT_ZOOM_API_SECRET', ''),
+    'nishant_zoom_api_email' => env('NISHANT_ZOOM_API_EMAIL', ''),
+];
